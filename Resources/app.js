d7a5981a33335d06e049eec4464bab7ab6749226
Dan Tamas <dan.tamas@gmail.com>
2012-08-15 10:51:36 +0200

    Fixed for android Update the sdk to 2.1.1 Small UI tweaks -  like use of dp and colors to make it pretty on android

--- Resources/app.js
+++ Resources/app.js
@@ -1,8 +1,11 @@
-var win = Ti.UI.createWindow();
+var win = Ti.UI.createWindow({
+	backgroundColor:'#fff'
+});
 
 var lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 var tbl = Ti.UI.createTableView({
+	backgroundColor:'#fff',
 	minRowHeight: 70,
 	selectionStyle: 'none'
 });
@@ -13,14 +16,14 @@ var make_actions_view = function(where) { // create the actions view - the one w
 	
 	var view = Ti.UI.createView({
 		backgroundColor: '#fff',
-		height: 70
+		height: '70dp'
 	});
 
 	for (var i = 0; i < 6; i++) {
 		view.add(Ti.UI.createImageView({
-			width: 50,
-			left: 10 + (50 * i),
-			height: 50,
+			width: '50dp',
+			left: (10 + (50 * i))+'dp',
+			height: '50dp',
 			image: 'KS_nav_ui.png',
 			is_action: 'action ' + i
 		}));
@@ -37,18 +40,19 @@ var make_content_view = function(chars) {// create the content view - the one is
 	});
 
 	var img = Ti.UI.createImageView({
-		height: 40,
-		width: 40,
-		left: 5,
-		top: 5,
+		height: '40dp',
+		width: '40dp',
+		left: '5dp',
+		top: '5dp',
 		image: 'https://si0.twimg.com/profile_images/2179402304/appc-fb_normal.png'
 	});
 
 	var label = Ti.UI.createLabel({
 		text: lorem.slice(0, chars),
+		color:'#000',
 		top: 0,
-		left: 50,
-		width: 260,
+		left: '50dp',
+		width: '260dp',
 		height: Ti.UI.SIZE
 	});
 
@@ -69,13 +73,36 @@ function make_data_rows() { // some stub data for the rows.
 		var randVal = parseInt(50 + (Math.random() * (lorem.length - 50)), 10);
 
 		var row = Ti.UI.createTableViewRow({
-			height: Ti.UI.SIZE
+			height: Ti.UI.SIZE,
+			backgroundColor:'#fff'
 		});
 
 		var v1 = make_actions_view();
 		row.v2 = make_content_view(randVal);
 		row.add(v1);
 		row.add(row.v2);
+
+
+		// android behaves in a different way so we need to add the event to the row.
+		
+		row.addEventListener('swipe', function(e) {
+			
+			if (!!current_row) {
+				current_row.v2.animate({
+					opacity: 1,
+					duration: 500
+				});
+			};
+
+			current_row = Ti.Platform.osname == 'android' ? this : e.row; // it looks like android does not have the e.row property for this event.
+
+			current_row.v2.animate({
+				opacity: 0,
+				duration: 500
+			});
+		});
+
+
 
 		data.push(row);
 	};
@@ -84,23 +111,6 @@ function make_data_rows() { // some stub data for the rows.
 
 make_data_rows();
 
-
-// events
-
-tbl.addEventListener('swipe', function(e) {
-	if (current_row) {
-		current_row.v2.animate({
-			opacity: 1,
-			duration: 500
-		});
-	};
-
-	current_row = e.row;
-	current_row.v2.animate({
-		opacity: 0,
-		duration: 500
-	});
-});
 
 tbl.addEventListener('scroll', function(e) {
 
